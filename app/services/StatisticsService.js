f2d867952ed2c67c196bf4170fae23cb4833f3c0
Hortense Cauchois <niels13693@gmail.com>
2024-04-26 13:48:31 +0200

    Service feature: added getMusic service function

--- app/services/StatisticsService.js
+++ app/services/StatisticsService.js
@@ -19,4 +19,21 @@ export default class StatisticsService {
         });
     }
 
+    static async getMusic(user, rank) {
+        const prisma = new PrismaClient();
+        if (rank < 1 || rank > 3) {
+            throw Error(`invalid rank :${rank} greater than 3 or less than 1`);
+        }
+        return prisma.topListenedMusic.findFirst({
+           where: {
+               AND: {
+                   top_ranking: rank,
+                   userMusicStatistic: {
+                       user_id: user.id
+                   }
+               }
+           }
+        });
+    }
+
 }
