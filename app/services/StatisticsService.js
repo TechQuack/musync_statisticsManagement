a1dd9e01be3138342866873a5fe77f1e00372ab5
Hortense Cauchois <niels13693@gmail.com>
2024-04-26 13:44:21 +0200

    Service feature: added getArtist service function

---
+++ app/services/StatisticsService.js
@@ -0,0 +1,22 @@
+import {PrismaClient, User, TopListenedArtist, UserMusicStatistic, TopListenedMusic} from '@prisma/client';
+
+export default class StatisticsService {
+
+    static async getArtist(user, rank) {
+        const prisma = new PrismaClient();
+        if (rank < 1 || rank > 3) {
+            throw Error(`invalid rank :${rank} greater than 3 or less than 1`);
+        }
+        return prisma.topListenedArtist.findFirst({
+            where: {
+                AND: {
+                    top_ranking: rank,
+                    userMusicStatistic: {
+                        user_id: user.id
+                    }
+                }
+            }
+        });
+    }
+
+}
